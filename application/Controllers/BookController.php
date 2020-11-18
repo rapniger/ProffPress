40f352d5f977c2f36e963113f46e570587b8694f
unknown <rapniger@yandex.ru>
2020-11-18 20:28:50 +0300

    Удаление комментарии

--- application/Controllers/BookController.php
+++ application/Controllers/BookController.php
@@ -120,27 +120,4 @@ class BookController extends FrontendController
     {
         return unserialize($category);
     }
-
-/*
-    public function indexAction() 
-    {        
-        $this->view->set([
-            'title' => 'Blog',
-        ]);          
-        $this->view->render('index');
-    }   
-    
-    public function viewBlogAction($userName) 
-    {        
-        echo __FILE__ . ' - ' . __METHOD__ . '<br>';
-        var_dump($userName);
-    }    
-    
-    public function viewPostAction($userName, $postId) 
-    {        
-        echo __FILE__ . ' - ' . __METHOD__ . '<br>';
-        var_dump($userName);
-        var_dump($postId);
-    }        
-    */
 }
